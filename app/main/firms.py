6b16d97fcbb623937e73be998ce37099cfe133dc
Padam Sethia <padamsethia5@gmail.com>
2019-09-26 17:59:29 +0530

    Base firms view complete

--- app/main/firms.py
+++ app/main/firms.py
@@ -5,19 +5,27 @@ from app.main import bp
 from app.master.model import Company, CompanySchema
 from app.employee.model import Employee
 from app import db
-
+import json
 
 @bp.route('/firms', methods=['GET', 'POST'])
 @login_required
 def show_firms():
-    return render_template('reports/firms.html', title=('Home'))
+    return render_template('reports/firms.html', title=('Firms'))
 
 
 @bp.route('/firms/info', methods=['GET', 'POST'])
 @login_required
 def firms_info():
     data_schema = CompanySchema(many=True)
-    data = db.session.query(Company).first().emp_company
+    data = db.session.query(Company).join(Employee.company).all()
     json_dump = data_schema.dumps(data)
-    print(json_dump)
-    return jsonify({'message': json_dump})
+    temp_json = json.loads(json_dump)
+    for item in temp_json:
+        all_emp = db.session.query(Company).filter_by(id=int(item['id'])).first().emp_company
+        item['total_emp'] = len(all_emp)
+        item['total_pay'] = 0
+        for emp in all_emp:
+            item['total_pay'] += float(emp.basicpay)
+
+    json_dump = json.dumps(temp_json)
+    return jsonify(json_dump)
--- app/static/js/reports/firms.js
+++ app/static/js/reports/firms.js
@@ -3,6 +3,27 @@ new Vue({
     el: "#firms_template",
     data() {
 
-        return {}
+        return {
+            firms: []
+
+        }
+    },
+    delimiters: ["[[", "]]"],
+    mounted() {
+
+        let raw = this
+        axios.post('/firms/info').
+            then(function (response) {
+                raw.firms = JSON.parse(response.data)
+            })
+
+    },
+    methods: {
+        formatedNumber(val) {
+            let test = Number(val).toLocaleString('en-IN', { style: 'currency', currency: 'INR' });
+            console.log(test)
+            return test
+        },
     }
+
 })
