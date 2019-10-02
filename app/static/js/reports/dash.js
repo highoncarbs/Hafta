8519873705129afb46f19062a51144aded66d6d0
Padam Sethia <padamsethia5@gmail.com>
2019-10-02 22:25:27 +0530

    dummy dashboard template

---
+++ app/static/js/reports/dash.js
@@ -0,0 +1,30 @@
+
+new Vue({
+    el: "#dash_template",
+    data() {
+
+        return {
+            firms: []
+
+        }
+    },
+    delimiters: ["[[", "]]"],
+    mounted() {
+
+        let raw = this
+        axios.post('/firms/info').
+            then(function (response) {
+                raw.firms = JSON.parse(response.data)
+            })
+
+    },
+    methods: {
+        formatedNumber(val) {
+            let test = Number(val).toLocaleString('en-IN', { style: 'currency', currency: 'INR' });
+            console.log(test)
+            return test
+        },
+    }
+
+})
+Vue.component('vue-feather', VueFeather);
