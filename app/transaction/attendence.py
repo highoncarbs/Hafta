3e7f90c8412ad0b0ebfa808f59e7da2c88920ca0
Padam Sethia <padamsethia5@gmail.com>
2019-10-12 16:26:15 +0530

    Added Attendence Summary API

--- app/transaction/attendence.py
+++ app/transaction/attendence.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask import render_template, redirect, url_for, request, session, jsonify
 from flask_login import login_user, logout_user, current_user
 from app.transaction import bp
-from app.transaction.model_att import Attendence, AttendenceSchema
+from app.transaction.model_att import Attendence, AttendenceSchema , CompanySchema
 from app.employee.model import Employee
 from app.master.model import Company
 
@@ -58,30 +58,66 @@ def emp_attendence(emp_id):
         return jsonify({'message': 'Invalid HTTP request method.'})
 
 
-@bp.route('/attendence/employee/data/<emp_id>', methods=['POST'])
-def emp_attendence_data(emp_id):
+# @bp.route('/attendence/employee/data/<emp_id>', methods=['POST'])
+# def emp_attendence_data(emp_id):
+#     if request.method == "POST":
+#         data = Attendence.query.filter(
+#             Attendence.employee.any(Employee.id == int(emp_id))).all()
+#         # data_schema = AttendenceSchema(many=True)
+#         today = datetime.now()
+#         today.year()
+#         return jsonify(json_data)
+#     else:
+#         return jsonify({'message': 'Invalid HTTP request method.'})
+
+
+@bp.route('/attendence/summary/latecomin', methods=['POST'])
+def summary_late_attendence():
     if request.method == "POST":
-        data = Attendence.query.filter(
-            Attendence.employee.any(Employee.id == int(emp_id))).all()
-        # data_schema = AttendenceSchema(many=True)
+        # Setting fiscal Year
         today = datetime.now()
-        today.year()
-        return jsonify(json_data)
+        payload_date = datetime(
+            int(today.year), int(1), int(1))
+        payload_date_end = datetime(
+            int(today.year + 1), int(1), int(1))
+        all_emps = Employee.query.filter(Employee.flag == 0).all()
+        payload = {}
+        payload_late = {}
+        payload_early = {}
+        for emp in all_emps:
+            data = Attendence.query.filter(Attendence.employee.any(Employee.id == int(emp.id)),
+                                           Attendence.date >= payload_date, Attendence.date <= payload_date_end).all()
+
+            day_att = 0
+            early_att = 0
+            late_att = 0
+            for item in data:
+                day_att += item.daysatt
+                early_att += item.earlygoing
+                late_att += item.latecomin
+            company_schema = CompanySchema(many=True) 
+            payload_data = {'name': emp.name, 'company': company_schema.dumps(emp.company), 'day_att': day_att, 'early_att': early_att, 'late_att': late_att}
+            payload_late.update({emp.id: payload_data})
+        payload_late = sorted(payload_late.items(), key = lambda x : x[1]['late_att'])[::-1][:5]
+        payload_early = sorted(payload_early.items(), key = lambda x : x[1]['early_att'])[::-1][:5]
+        payload['early'] = payload_early
+        payload['late'] = payload_late
+        return jsonify(payload)
     else:
         return jsonify({'message': 'Invalid HTTP request method.'})
 
 
-@bp.route('/attendence/save', methods=['POST'])
+@bp.route('/attendence/save', methods = ['POST'])
 def save_attendence():
     if request.method == 'POST':
-        payload = request.json
+        payload=request.json
         if payload != None:
-            payload_data = payload['data']
-            payload_date = payload['date'].split('-')
-            payload_date = datetime(
+            payload_data=payload['data']
+            payload_date=payload['date'].split('-')
+            payload_date=datetime(
                 int(payload_date[0]), int(payload_date[1]), int(1))
             # Date checks to be done
-            table_columns = (
+            table_columns=(
                 'daysatt',
                 'latecomin',
                 'earlygoing'
@@ -91,15 +127,15 @@ def save_attendence():
 
                 # Need Update cehck inside
                 for item in payload_data:
-                    new_data = Attendence()
-                    emp = Employee.query.filter_by(
-                        id=int(item['id'])).first()
-                    company = Company.query.filter_by(
-                        id=int(payload['company'])).first()
+                    new_data=Attendence()
+                    emp=Employee.query.filter_by(
+                        id = int(item['id'])).first()
+                    company=Company.query.filter_by(
+                        id = int(payload['company'])).first()
                     new_data.company.append(company)
                     new_data.employee.append(emp)
                     for field in table_columns:
-                        val = item[field]
+                        val=item[field]
                         if val == '' or val is None:
                             continue
                         setattr(new_data, field, val)
@@ -108,9 +144,9 @@ def save_attendence():
                     if 'tdsval' in item.keys():
                         if item['tdsval'] != "":
                             setattr(new_data, 'tds', item['tdsval'])
-                    
+
                     if 'other_deduction' in item.keys():
-                        val = item['other_deduction']
+                        val=item['other_deduction']
                         if val == '' or val is None:
                             continue
                         setattr(new_data, 'other_deduction',
@@ -138,13 +174,13 @@ def save_attendence():
             return jsonify({'message': 'Empty data.'})
 
 
-@bp.route('/attendence/update', methods=['POST'])
+@bp.route('/attendence/update', methods = ['POST'])
 def update_attendence():
     if request.method == 'POST':
-        payload = request.json
+        payload=request.json
         if payload != None:
-          
-            table_columns = (
+
+            table_columns=(
                 'daysatt',
                 'latecomin',
                 'earlygoing'
